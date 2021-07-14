636c80eef668c1ce6033641a58449fde42b7cb98
Yusuke0609 <79389429+Yusuke0609@users.noreply.github.com>
2021-07-14 20:37:44 +0900

    削除

--- env.php
+++
@@ -1,9 +0,0 @@
-<?php
-//DBの設定値を入力
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'workshop');
-define('DB_USER', 'Yamauchi');
-define('DB_PASS', 'yusuke0609');
-define('DB_PORT', '3306');
-?>
